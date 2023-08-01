1775c0863187e0956a8b4ef12cd9932b16515319
SahinS9 <shahinsaatov@gmail.com>
2023-08-01 15:48:53 +0400

    before single page js implementation

---
+++ day7/django/singlepage/singlepage1/views.py
@@ -0,0 +1,21 @@
+from django.http import Http404, HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'singlepage1/index.html')
+
+
+texts = [
+    'Text 1 for the first Page',
+    'Text 2 shows page 2 is here',
+    'Text 3 identifies 3 page with example'
+]
+
+
+def section(request, num):
+    if 1<= num <= 3:
+        return HttpResponse(texts[num-1])
+    else:
+        raise Http404('No Such Section')
